3e8c98a92a3e5b61652fb84ede6f9bd5c186c0aa
yushi265 <ae265.1108@gmail.com>
2021-05-18 21:32:19 +0900

    未ログイン時ナビバー非表示

--- backend/resources/views/layouts/nav.blade.php
+++ backend/resources/views/layouts/nav.blade.php
@@ -7,67 +7,58 @@
             SFA
         </a>
 
-        <!-- Toggle button -->
-        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-            data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <i class="fas fa-bars"></i>
-        </button>
-
-        <!-- Collapsible wrapper -->
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/customers') }}">顧客管理</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/progresses') }}">進捗管理</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/contracts') }}">成約情報</a>
-                </li>
-                @can('system-only')
+        @auth
+            <!-- Toggle button -->
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+                data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <!-- Collapsible wrapper -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/admin') }}">権限管理</a>
+                        <a class="nav-link" href="{{ url('/customers') }}">顧客管理</a>
                     </li>
-                @endcan
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/weathers') }}">天気予報</a>
-                </li>
-
-                @guest
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('ログイン') }}</a>
+                        <a class="nav-link" href="{{ url('/progresses') }}">進捗管理</a>
                     </li>
-                    {{-- @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('登録') }}</a>
-                                </li>
-                            @endif --}}
-                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/contracts') }}">成約情報</a>
+                    </li>
+                    @can('system-only')
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/admin') }}">権限管理</a>
+                        </li>
+                    @endcan
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/weathers') }}">天気予報</a>
+                    </li>
+
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name . '(' . Auth::user()->role->name . ')' }}
-                            <span class="caret"></span>
-                        </a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                                     document.getElementById('logout-form').submit();">
-                                {{ __('ログアウト') }}
-                            </a>
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-mdb-toggle="dropdown" aria-expanded="false">
+                            {{ Auth::user()->name . '(' . Auth::user()->role->name . ')' }}
+                        </a>
+                        <!-- Dropdown menu -->
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                    {{ __('ログアウト') }}
+                                </a>
+                            </li>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
                             </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
+                        </ul>
+            </div>
         </div>
-        <!-- Collapsible wrapper -->
+    @endauth
+    <!-- Collapsible wrapper -->
     </div>
     <!-- Container wrapper -->
 </nav>
