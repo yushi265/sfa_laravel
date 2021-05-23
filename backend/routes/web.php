520c78e8552812672d82316f285db05428e63e50
yushi265 <ae265.1108@gmail.com>
2021-05-23 21:20:30 +0900

    ルーティングにname付与

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -18,52 +18,54 @@ Auth::routes();
 
 // システム管理者のみ
 Route::group(['middleware' => ['auth', 'can:system-only']], function () {
-    Route::get('/admin', 'UserController@admin_index');
-    Route::patch('/admin', 'UserController@admin_set');
+    Route::prefix('admin')->name('admin.')->group(function() {
+        Route::get('', 'UserController@admin_index')->name('index');
+        Route::patch('', 'UserController@admin_set')->name('set');
+    });
 });
 
 // 管理者以上
 Route::group(['middleware' => ['auth', 'can:admin-higher']], function () {
-    Route::prefix('customers/')->group(function () {
-        Route::get('create', 'CustomerController@create');
-        Route::post('', 'CustomerController@store');
-        Route::get('{customer}/edit', 'CustomerController@edit');
-        Route::patch('{customer}', 'CustomerController@update');
+    Route::prefix('customers/')->name('customers.')->group(function () {
+        Route::get('create', 'CustomerController@create')->name('create');
+        Route::post('', 'CustomerController@store')->name('store');
+        Route::get('{customer}/edit', 'CustomerController@edit')->name('edit');
+        Route::patch('{customer}', 'CustomerController@update')->name('update');
     });
 
-    Route::prefix('progresses/')->group(function () {
-        Route::get('{progress}/edit', 'ProgressController@edit');
-        Route::patch('{progress}', 'ProgressController@update');
-        Route::delete('{progress}', 'ProgressController@destroy');
+    Route::prefix('progresses/')->name('progresses.')->group(function () {
+        Route::get('{progress}/edit', 'ProgressController@edit')->name('edit');
+        Route::patch('{progress}', 'ProgressController@update')->name('update');
+        Route::delete('{progress}', 'ProgressController@destroy')->name('destroy');
     });
 
-    Route::prefix('contracts/')->group(function () {
-        Route::get('create', 'ContractController@create');
-        Route::post('', 'ContractController@store');
-        Route::get('{contract}/edit', 'ContractController@edit');
-        Route::patch('{contract}', 'ContractController@update');
-        Route::delete('{contract}', 'ContractController@destroy');
+    Route::prefix('contracts/')->name('contracts.')->group(function () {
+        Route::get('create', 'ContractController@create')->name('create');
+        Route::post('', 'ContractController@store')->name('store');
+        Route::get('{contract}/edit', 'ContractController@edit')->name('edit');
+        Route::patch('{contract}', 'ContractController@update')->name('update');
+        Route::delete('{contract}', 'ContractController@destroy')->name('destroy');
     });
 });
 
 // 全ユーザ
 Route::group(['middleware' => ['auth', 'can:user-higher']], function () {
-    Route::get('/', 'UserController@home');
+    Route::get('/', 'UserController@home')->name('home');
 
-    Route::prefix('customers/')->group(function () {
-        Route::get('', 'CustomerController@index');
-        Route::get('{customer}', 'CustomerController@show');
+    Route::prefix('customers/')->name('customers.')->group(function () {
+        Route::get('', 'CustomerController@index')->name('index');
+        Route::get('{customer}', 'CustomerController@show')->name('show');
     });
 
-    Route::prefix('progresses/')->group(function () {
-        Route::get('', 'ProgressController@index');
-        Route::get('create', 'ProgressController@create');
-        Route::post('', 'ProgressController@store');
+    Route::prefix('progresses/')->name('progresses.')->group(function () {
+        Route::get('', 'ProgressController@index')->name('index');
+        Route::get('create', 'ProgressController@create')->name('create');
+        Route::post('', 'ProgressController@store')->name('store');
     });
 
-    Route::prefix('contracts/')->group(function () {
-        Route::get('', 'ContractController@index');
+    Route::prefix('contracts/')->name('contracts.')->group(function () {
+        Route::get('', 'ContractController@index')->name('index');
     });
 
-    Route::get('/weathers', 'WeatherController@index');
+    Route::get('/weathers', 'WeatherController@index')->name('weather');
 });
